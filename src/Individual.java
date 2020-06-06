e88b4f84cc248129f62d381f453860c014158c64
Stephen Jordan <98.stephenjordan@gmail.com>
2020-06-06 07:40:11 +0700

    TSP Problem

--- src/Individual.java
+++ src/Individual.java
@@ -1,34 +1,130 @@
-import java.util.Random;
+public class Individual {
 
-//Individual class
-class Individual {
+    /**
+     * In this case, the chromosome is an array of integers rather than a string.
+     */
+    private int[] chromosome;
+    private double fitness = -1;
 
-    int fitness = 0;
-    int[] genes = null;
-    int geneLength = 0;
-
-    public Individual(int geneLength) {
-        Random rn = new Random();
-        this.genes = new int[geneLength];
-        this.geneLength = geneLength;
-
-        //Set genes randomly for each individual
-        for (int i = 0; i < genes.length; i++) {
-            genes[i] = Math.abs(rn.nextInt() % 2); //Agar nilai gen ada pada rentang nilai [0,1]
-        }
-
-        fitness = 0;
+    /**
+     * Initializes individual with specific chromosome
+     *
+     * @param chromosome
+     *            The chromosome to give individual
+     */
+    public Individual(int[] chromosome) {
+        // Create individualchromosome
+        this.chromosome = chromosome;
     }
 
-    //Calculate fitness
-    public void calcFitness() {
+    /**
+     * Initializes random individual
+     *
+     * @param chromosomeLength
+     *            The length of the individuals chromosome
+     */
+    public Individual(int chromosomeLength) {
+        // Create random individual
+        int[] individual;
+        individual = new int[chromosomeLength];
 
-        fitness = 0;
-        for (int i = 0; i < geneLength; i++) {
-            if (genes[i] == 1) {
-                ++fitness;
+        /**
+         * In this case, we can no longer simply pick 0s and 1s -- we need to
+         * use every city index available. We also don't need to randomize or
+         * shuffle this chromosome, as crossover and mutation will ultimately
+         * take care of that for us.
+         */
+        for (int gene = 0; gene < chromosomeLength; gene++) {
+            individual[gene] = gene;
+        }
+
+        this.chromosome = individual;
+    }
+
+    /**
+     * Gets individual's chromosome
+     *
+     * @return The individual's chromosome
+     */
+    public int[] getChromosome() {
+        return this.chromosome;
+    }
+
+    /**
+     * Gets individual's chromosome length
+     *
+     * @return The individual's chromosome length
+     */
+    public int getChromosomeLength() {
+        return this.chromosome.length;
+    }
+
+    /**
+     * Set gene at offset
+     *
+     * @param gene
+     * @param offset
+     */
+    public void setGene(int offset, int gene) {
+        this.chromosome[offset] = gene;
+    }
+
+    /**
+     * Get gene at offset
+     *
+     * @param offset
+     * @return gene
+     */
+    public int getGene(int offset) {
+        return this.chromosome[offset];
+    }
+
+    /**
+     * Store individual's fitness
+     *
+     * @param fitness
+     *            The individuals fitness
+     */
+    public void setFitness(double fitness) {
+        this.fitness = fitness;
+    }
+
+    /**
+     * Gets individual's fitness
+     *
+     * @return The individual's fitness
+     */
+    public double getFitness() {
+        return this.fitness;
+    }
+
+    public String toString() {
+        String output = "";
+        for (int gene = 0; gene < this.chromosome.length; gene++) {
+            output += this.chromosome[gene] + ",";
+        }
+        return output;
+    }
+
+    /**
+     * Search for a specific integer gene in this individual.
+     *
+     * For instance, in a Traveling Salesman Problem where cities are encoded as
+     * integers with the range, say, 0-99, this method will check to see if the
+     * city "42" exists.
+     *
+     * @param gene
+     * @return
+     */
+    public boolean containsGene(int gene) {
+        for (int i = 0; i < this.chromosome.length; i++) {
+            if (this.chromosome[i] == gene) {
+                return true;
             }
         }
+        return false;
     }
+
+
 
 }
--- src/Population.java
+++ src/Population.java
@@ -1,71 +1,139 @@
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
 
-//Population class
-class Population {
+public class Population {
+    private Individual population[];
+    private double populationFitness = -1;
 
-    int popSize = 10;
-    Individual[] individuals = new Individual[10];
-    int fittest = 0;
-    String genes_fittest = "";
+    /**
+     * Initializes blank population of individuals
+     *
+     * @param populationSize
+     *            The size of the population
+     */
+    public Population(int populationSize) {
+        // Initial population
+        this.population = new Individual[populationSize];
+    }
 
+    /**
+     * Initializes population of individuals
+     *
+     * @param populationSize
+     *            The size of the population
+     * @param chromosomeLength
+     *            The length of the individuals chromosome
+     */
+    public Population(int populationSize, int chromosomeLength) {
+        // Initial population
+        this.population = new Individual[populationSize];
 
-    //Initialize population
-    public void initializePopulation(int size, int genesLength) {
-        for (int i = 0; i < individuals.length; i++) {
-            individuals[i] = new Individual(genesLength);
+        // Loop over population size
+        for (int individualCount = 0; individualCount < populationSize; individualCount++) {
+            // Create individual
+            Individual individual = new Individual(chromosomeLength);
+            // Add individual to population
+            this.population[individualCount] = individual;
         }
     }
 
-    //Get the fittest individual
-    public Individual getFittest() {
-        int maxFit = Integer.MIN_VALUE;
-        int maxFitIndex = 0;
-        for (int i = 0; i < individuals.length; i++) {
-            if (maxFit <= individuals[i].fitness) {
-                maxFit = individuals[i].fitness;
-                maxFitIndex = i;
+    /**
+     * Get individuals from the population
+     *
+     * @return individuals Individuals in population
+     */
+    public Individual[] getIndividuals() {
+        return this.population;
+    }
+
+    /**
+     * Find fittest individual in the population
+     *
+     * @param offset
+     * @return individual Fittest individual at offset
+     */
+    public Individual getFittest(int offset) {
+        // Order population by fitness
+        Arrays.sort(this.population, new Comparator<Individual>() {
+            @Override
+            public int compare(Individual o1, Individual o2) {
+                if (o1.getFitness() > o2.getFitness()) {
+                    return -1;
+                } else if (o1.getFitness() < o2.getFitness()) {
+                    return 1;
+                }
+                return 0;
             }
-        }
-        fittest = individuals[maxFitIndex].fitness;
-        genes_fittest = Arrays.toString(individuals[maxFitIndex].genes);
-        return individuals[maxFitIndex];
+        });
+
+        // Return the fittest individual
+        return this.population[offset];
     }
 
-    //Get the second most fittest individual
-    public Individual getSecondFittest() {
-        int maxFit1 = 0;
-        int maxFit2 = 0;
-        for (int i = 0; i < individuals.length; i++) {
-            if (individuals[i].fitness > individuals[maxFit1].fitness) {
-                maxFit2 = maxFit1;
-                maxFit1 = i;
-            } else if (individuals[i].fitness > individuals[maxFit2].fitness) {
-                maxFit2 = i;
-            }
-        }
-        return individuals[maxFit2];
+    /**
+     * Set population's fitness
+     *
+     * @param fitness
+     *            The population's total fitness
+     */
+    public void setPopulationFitness(double fitness) {
+        this.populationFitness = fitness;
     }
 
-    //Get index of least fittest individual
-    public int getLeastFittestIndex() {
-        int minFitVal = Integer.MAX_VALUE;
-        int minFitIndex = 0;
-        for (int i = 0; i < individuals.length; i++) {
-            if (minFitVal >= individuals[i].fitness) {
-                minFitVal = individuals[i].fitness;
-                minFitIndex = i;
-            }
-        }
-        return minFitIndex;
+    /**
+     * Get population's fitness
+     *
+     * @return populationFitness The population's total fitness
+     */
+    public double getPopulationFitness() {
+        return this.populationFitness;
     }
 
-    //Calculate fitness of each individual
-    public void calculateFitness() {
+    /**
+     * Get population's size
+     *
+     * @return size The population's size
+     */
+    public int size() {
+        return this.population.length;
+    }
 
-        for (int i = 0; i < individuals.length; i++) {
-            individuals[i].calcFitness();
+    /**
+     * Set individual at offset
+     *
+     * @param individual
+     * @param offset
+     * @return individual
+     */
+    public Individual setIndividual(int offset, Individual individual) {
+        return population[offset] = individual;
+    }
+
+    /**
+     * Get individual at offset
+     *
+     * @param offset
+     * @return individual
+     */
+    public Individual getIndividual(int offset) {
+        return population[offset];
+    }
+
+    /**
+     * Shuffles the population in-place
+     *
+     * @param void
+     * @return void
+     */
+    public void shuffle() {
+        Random rnd = new Random();
+        for (int i = population.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            Individual a = population[index];
+            population[index] = population[i];
+            population[i] = a;
         }
-        getFittest();
     }
 
 }
